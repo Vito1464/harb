cd8bac0a9f73f472af4212e8cc7112e64ef03f44
Axel <axel@Axels-MacBook-Pro.local>
2026-04-07 04:26:07 +0100

    Restore data sync with fresh endpoints

--- api/sync.js
+++ api/sync.js
@@ -2,8 +2,8 @@ export default async function handler(req, res) {
   const { db } = req.query;
   
   const blobInfo = {
-    actors: '019d6038-22d0-771f-a49c-9956b8edd9f4',
-    campaign: '019d6038-2a7a-732c-b8ee-50e5800a8e94'
+    actors: '019d65f5-d5d9-7686-8d37-cc3fa0442e58',
+    campaign: '019d65f8-2afe-72f2-a3fa-e6d5b48bf0d0'
   };
   
   const id = blobInfo[db];
